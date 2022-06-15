812e038663897ef964f04ff0a1e131d308edf9e3
AlvitoDn <alvtdwnv18@gmail.com>
2022-06-15 13:11:12 +0700

    AppbiodataAlvito.py

--- AppbiodataAlvito/app/Config/Routes.php
+++ AppbiodataAlvito/app/Config/Routes.php
@@ -32,6 +32,8 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/test', 'Home::index2');
+$routes->get('/al','coba::index3');
 
 /*
  * --------------------------------------------------------------------
